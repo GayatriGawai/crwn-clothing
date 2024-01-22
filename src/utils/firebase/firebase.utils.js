5881166c2dc179c41c0902dab5471193724c1960
shaileshminsnapsys <shailesh.m.insnapsys@gmail.com>
2024-01-22 11:34:40 +0530

    Added pur sign in component as well as firebase utils and brought in firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,6 +10,8 @@ import {
     GoogleAuthProvider,
 } from 'firebase/auth';
 
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: 'AIzaSyB8omq6JBOm_Zk8Qceo2h9wUvvp5Gi_jsw',
@@ -31,3 +33,30 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+// the DB instance allows us to tell the firebase when we want to get a document or
+// we want to set the document related to the databse
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(44, userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(48, userSnapshot);
+    console.log(49, userSnapshot.exists());
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt });
+        } catch (error) {
+            console.log('Error while creating the user', error.message);
+        }
+    }
+    return userDocRef;
+};
