fc73b58f3b7f3cd0ff44ce951ab26fe226d5d9e9
shaileshminsnapsys <shailesh.m.insnapsys@gmail.com>
2024-01-31 15:51:58 +0530

    Deploying on netlify

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,14 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import {
     DirectoryItemContainer,
     BackgroundImage,
     Body,
 } from './directory-item.styles';
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
 
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
     return (
-        <DirectoryItemContainer>
-            <BackgroundImage imageUrl={imageUrl} />
+        <DirectoryItemContainer onClickCapture={onNavigateHandler}>
+            <BackgroundImage imageurl={imageUrl} />
             <Body>
                 <h2>{title}</h2>
                 <p>Shop now</p>
