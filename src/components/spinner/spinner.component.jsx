8555cc5a12e6521c61d746b3ac65136f7a51e5c9
shaileshminsnapsys <shailesh.m.insnapsys@gmail.com>
2024-02-07 15:31:56 +0530

    Updated thunk pattern for fetching the categories

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,12 @@
+import { SpinnerContainer, SpinnerOverlay } from './spinner.styles';
+
+const Spinner = () => {
+    console.log('Spinner');
+    return (
+        <SpinnerContainer>
+            <SpinnerOverlay />
+        </SpinnerContainer>
+    );
+};
+
+export default Spinner;
--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,25 +1,33 @@
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
-
+import {
+    selectCategoriesMap,
+    selectIsLoading,
+} from '../../store/categories/categories.selector';
+import Spinner from '../../components/spinner/spinner.component';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 const CategoriesPreview = () => {
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectIsLoading);
 
     return (
         <Fragment>
-            {Object.keys(categoriesMap).map((title) => {
-                const products = categoriesMap[title];
-                return (
-                    <CategoryPreview
-                        key={title}
-                        title={title}
-                        products={products}
-                    />
-                );
-            })}
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                Object.keys(categoriesMap).map((title) => {
+                    const products = categoriesMap[title];
+                    return (
+                        <CategoryPreview
+                            key={title}
+                            title={title}
+                            products={products}
+                        />
+                    );
+                })
+            )}
         </Fragment>
     );
 };
